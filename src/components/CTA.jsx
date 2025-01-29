a279fbfe952d34b6eb29754dd89c31f371909dd4
Kgotso Makhalimele <89485688+kgotsosm@users.noreply.github.com>
2025-01-29 21:20:18 +0200

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -16,7 +16,6 @@ const CTA = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-
   return (
     <section id='contact' className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className='flex-1 flex flex-col'>
